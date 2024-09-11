fab7ba674322a6819c2268ffb600aaf96188a29f
Isha Savani <123747477+Ishasavani1402@users.noreply.github.com>
2024-09-11 13:45:02 +0530

    Add files via upload

---
+++ arrayleader.java
@@ -0,0 +1,59 @@
+// Given an array arr of n positive integers, 
+// your task is to find all the leaders in the array.
+//  An element of the array is considered a leader 
+//  if it is greater than all the elements on its right side or
+//   if it is equal to the maximum element on its right side. 
+//   The rightmost element is always a leader.
+
+// Examples
+
+// Input: n = 6, arr[] = {16,17,4,3,5,2}
+// Output: 17 5 2
+// Explanation: Note that there is nothing greater on the right side of 17, 5 and, 2.
+// Input: n = 5, arr[] = {10,4,2,4,1}
+// Output: 10 4 4 1
+// Explanation: Note that both of the 4s are in output, as to be a leader an equal element is also allowed on the right. side
+// Input: n = 4, arr[] = {5, 10, 20, 40} 
+// Output: 40
+// Explanation: When an array is sorted in increasing order, only the rightmost element is leader.
+// Input: n = 4, arr[] = {30, 10, 10, 5} 
+// Output: 30 10 10 5
+// Explanation: When an array is sorted in non-increasing order, all elements are leaders.
+// Expected Time Complexity: O(n)
+// Expected Auxiliary Space: O(n)
+
+// Constraints:
+// 1 <= n <= 107
+// 0 <= arr[i] <= 107
+
+import java.util.*;
+public class arrayleader {
+
+    static ArrayList<Integer> leader(int arr[] , int n){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int last = arr[n-1];
+        ans.add(last);
+        for(int i=n-2;i>=0;i--){
+            if(arr[i] >= last){
+                ans.add(arr[i]);
+                last = arr[i];
+                // compare = arr[i];
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        ArrayList<Integer> a = leader(arr, n);
+        for(int i : a){
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
